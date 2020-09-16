305db0a84346dbe166708e2a972ed1dba3079b34
altryx <lampa@altx.dev>
2020-09-15 20:54:57 -0600

    Added unary and binary operator overloads for vector addition, subtraction and scalar multiplication and division, with appropriate test code;

--- OperatorOverloadDemo/Program.cs
+++ OperatorOverloadDemo/Program.cs
@@ -8,21 +8,64 @@ namespace OperatorOverloadDemo
         {
             Vector myTestVector = new Vector(2, 3);
 
+            // Get the pretty print version of the myTestVector
             Console.WriteLine(myTestVector.ToString());
+
+            // Define two new vectors to demonstrate different vector operations
+            Vector vector1 = new Vector(1, 1);
+            Vector vector2 = new Vector(3, 4);
+
+            // Vector addition
+            Vector vecAddition = vector1 + vector2;
+            Console.WriteLine($"{vector1} + {vector2} = {vecAddition}");
+
+            // What about ++ and -- operators?
+                // Console.WriteLine(vector1++);
+                // Console.WriteLine(vector1--);
+            // Won't compile: operator '++' cannot be applied to operand of type 'Program.Vector'
+            // This is because addition/subtraction of an integer to a vector is not defined in the struct 
+            // (and has no mathematical interpretation either)
+           
+
+            // Vector subtraction
+            Vector vecSubtraction = vector1 - vector2;
+            Console.WriteLine($"{vector1} - {vector2} = {vecSubtraction}");
+
+            // Multiplication of a vector by a scalar (i.e. constant)
+            Vector vecScalarMultiplication = vector1 * 3;
+            Console.WriteLine($"{vector1} * 3 = {vecScalarMultiplication}");
+
+            // Division of a vector by a scalar. Need to explicitly cast float as decimal here
+            Vector vecScalarDivision = vector1 / (decimal)3.5;
+            Console.WriteLine($"{vector1} / 3.5 = {vecScalarDivision}");
+            
+
+
         }
+
+
+
+
+
+
+
 
         private void SimpleExamples()
         {
             // Declare and assign variables containing primitive types
-            // Ref.
             int myInteger = 6;
+
+            // Still an int, only left to the compiler to determine on compilation
+            var implicitInteger = 6; 
+
             string myString = "Hello World!";
             
             // Simple integer addition
             int mySecondInteger = myInteger + 1;
 
             // Types can be automatically cast from one type to another,
-            // if defined as such
+            // if automatic typecasting is defined for the specific pairs
+            // (double and int in this case)
             double myDouble = mySecondInteger + 2 * myInteger / 9;
 
             // The above line is functionally equivalent to:
@@ -42,20 +85,31 @@ namespace OperatorOverloadDemo
         public struct Vector
         {
             // By making these private, a vector can only be defined at time of creation.
-            private int Magnitude, Direction;
+            private decimal X, Y;
 
             // Defining a vector through use of a constructor
-            public Vector(int magnitude, int direction)
+            public Vector(decimal x, decimal y)
             {
-                Magnitude = magnitude;
-                Direction = direction;
+                X = x;
+                Y = y;
             }
 
-            // Override generic object ToString() definition with our own in the form of "<magnitude,direction>"
-            public override string ToString() => $"<{Magnitude}, {Direction}>";
+            // Override generic ToString() definition with our own which returns 
+            // a string in the form of "<X,Y>"
+            public override string ToString() => $"<{X}, {Y}>";     
 
-            
+            // Define + and - operators when a single argument of type Vector is provided
+            public static Vector operator +(Vector vector) => vector;
+            public static Vector operator -(Vector vector) => new Vector(-vector.X, -vector.Y);
 
+            public static Vector operator +(Vector vector1, Vector vector2) => new Vector(vector1.X + vector2.X, vector1.Y + vector2.Y);
+            public static Vector operator -(Vector vector1, Vector vector2) => vector1+ (-vector2);
+
+            // Vector scalar multiplication
+            public static Vector operator *(Vector vector1, decimal scalar) => new Vector(vector1.X * scalar, vector1.Y * scalar);
+
+            // Vector scalar division
+            public static Vector operator /(Vector vector1, decimal scalar) => new Vector(vector1.X / scalar, vector1.Y / scalar);
             
         }
     }
